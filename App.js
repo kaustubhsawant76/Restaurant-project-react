1e46a55c4d6f6862a4333f6718b5485ccfcc0f44
Kaustubh Sawant <kaustubhsawant76@gmail.coom>
2024-06-30 16:53:37 +0530

    Project setup with git init

---
+++ App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const a=React.createElement("h1",{},"hi kaustubh sawant")
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(a)
