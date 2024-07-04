15a4f5d7640f9ccda488ce445a0c3278d94f7e8b
Kaustubh Sawant <kaustubhsawant76@gmail.coom>
2024-07-04 14:37:03 +0530

    id to map function

--- App.js
+++ App.js
@@ -25,13 +25,15 @@ const Header=()=>{
 
 const RestaurantCard=(props)=>{
     const{resData}=props;
+    const{name,cuisines,avgRating,cloudinaryImageId}=resData?.info;
+    const{deliveryTime}=resData?.info.sla;
     return (
         <div className="res-card" style={{backgroundColor:"#f0f0f0"}}>
-            <img className="image" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+resData.info.cloudinaryImageId }/>
-         <h3>{resData.info.name}</h3>
-         <h4>{resData.info.cuisines.join(",")}</h4>
-         <h5>{resData.info.avgRating} stars</h5>
-         <h5>{resData.info.sla.deliveryTime} minutes</h5>
+            <img className="image" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+cloudinaryImageId }/>
+         <h3>{name}</h3>
+         <h4>{cuisines.join(",")}</h4>
+         <h5>{avgRating} stars</h5>
+         <h5>{deliveryTime} minutes</h5>
         </div>
     );
 }
@@ -872,7 +874,7 @@ const Body=()=>{
         <div className="body">
             <div className="search">search</div>
             <div className="restaurant-container">
-             {resList.map((restaurant)=>(<RestaurantCard resData={restaurant} />))}
+             {resList.map((restaurant)=>(<RestaurantCard key={restaurant.info.id} resData={restaurant} />))}
              
             </div>
         </div>
