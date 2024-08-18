704958ab7f2bdebaa78bb8dbde4cd855e6705bfe
Kaustubh Sawant <kaustubhsawant76@gmail.coom>
2024-08-18 13:13:06 +0530

    class based use STATE

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,16 @@
+import User from "./User";
+import UserClass from "./UserClass";
 const About=()=>
 {
     return (
         <div>
             <h1>this is about</h1>
+            {/* <User name={"Kaustubh sawant (function)"}/>  */}
+
+            <UserClass name={"Kaustubh Sawant (class)"} Location={"Mumbai (Class)"}/>
         </div>
     )
 }
 
 export default About;
+//the above example shows props passing in functional components as well as class based components
