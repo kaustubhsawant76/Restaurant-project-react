ee41c2be65cb493cfc0b78ddd51c9c9d9346d6a3
Kaustubh Sawant <kaustubhsawant76@gmail.coom>
2024-09-10 13:00:25 +0530

    accordiance add feature

--- src/components/RestaurantCategories.js
+++ src/components/RestaurantCategories.js
@@ -1,15 +1,21 @@
+import Itemslist from "./ItemsList";
 
 const RestaurantCategories= ({data})=>{
- console.log(data);
+ //console.log(data);
  
     return(
       <div>
         {/* Header  */}
-        <div className="w-6/12 mx-auto my-6 bg-gray-50 shadow-lg p-4 flex justify-between ">
-            <span>{data.title}</span>
+        <div className="w-6/12 mx-auto my-6 bg-gray-100 shadow-lg p-4  ">
+         <div className="flex justify-between">
+         <span className="font-bold text-lg">{data.title} ({data.itemCards.length})</span>
             <span>⬇</span>
+            
+         </div>
+         <Itemslist items={data.itemCards}/>
         </div>
         {/* Accordion Body */}
+        
       </div>  
     );
 
