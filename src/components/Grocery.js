c371e7c309aac3a0af71fda42b41476f93230a11
Kaustubh Sawant <kaustubhsawant76@gmail.coom>
2024-10-05 17:07:09 +0530

    grouping test cases

--- src/components/Grocery.js
+++ src/components/Grocery.js
@@ -1,75 +1,14 @@
-import React, { useState } from 'react';
-import {
-    FaTh,
-    FaBars,
-    FaUserAlt,
-    FaRegChartBar,
-    FaCommentAlt,
-    FaShoppingBag,
-    FaThList
-}from "react-icons/fa";
-import { NavLink } from 'react-router-dom';
-import { Suspense } from 'react';
-import About from './About';
 
 
-const Grocery = ({children}) => {
-    const[isOpen ,setIsOpen] = useState(false);
-    const toggle = () => setIsOpen (!isOpen);
-    const menuItem=[
-        {
-            path:"/",
-            name:"Dashboard",
-            icon:<FaTh/>
-        },
-        {
-            path:"/about",
-            name:"About",
-            element:<Suspense fallback={<h1>Loading .......</h1>}><About/></Suspense> ,
-            icon:<FaUserAlt/>
-        },
-        {
-            path:"/analytics",
-            name:"Analytics",
-            icon:<FaRegChartBar/>
-        },
-        {
-            path:"/comment",
-            name:"Comment",
-            icon:<FaCommentAlt/>
-        },
-        {
-            path:"/product",
-            name:"Product",
-            icon:<FaShoppingBag/>
-        },
-        {
-            path:"/productList",
-            name:"Product List",
-            icon:<FaThList/>
-        }
-    ]
+
+const Grocery = () => {
+   
+      
+    
     return (
-        <div className="container flex m-0 p-0 text-decoration-line: none">
-           <div 
-          style={{width: isOpen ? "200px" : "50px"}} 
-           className="sidebar bg-black text-white h-{100vh} w-{200px} transition-all duration-500" >
-               <div className="top_section">
-                   <h1 style={{display: isOpen ? "block" : "none"}} className="logo">Logo</h1>
-                   <div style={{marginLeft: isOpen ? "50px" : "0px"}} className="bars">
-                       <FaBars onClick={toggle}/>
-                   </div>
-               </div>
-               {
-                   menuItem.map((item, index)=>(
-                       <NavLink to={item.path} key={index} className="link" activeclassName="active">
-                           <div className="icon">{item.icon}</div>
-                           <div style={{display: isOpen ? "block" : "none"}} className="link_text">{item.name}</div>
-                       </NavLink>
-                   ))
-               }
-           </div>
-           <main>{children}</main>
+        <div >
+           <h1> This is Grocery component</h1>
+         
         </div>
     );
 };
--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -2,35 +2,43 @@ import { render,screen } from "@testing-library/react";
 import Contact from "../Contact";
 import "@testing-library/jest-dom";
 
-test("Should load contact or not",()=>{
-    render(<Contact/>);
+describe("Contact us page test cases",() => {
+    test("Should load contact or not",()=>{
+        render(<Contact/>);
+    
+        const heading=screen.getByRole("heading");
+    
+        expect(heading).toBeInTheDocument();
+    })
+    
+    test("Should load contact or not",()=>{
+        render(<Contact/>);
+    
+        const button=screen.getByRole("button");
+    
+        expect(button).toBeInTheDocument();
+    })
+    
+    test("Should load contact or not",()=>{
+        render(<Contact/>);
+    
+        const button1=screen.getByText("Submit");
+    
+        expect(button1).toBeInTheDocument();
+    })
+    
+    test("Should load contact or not",()=>{
+        render(<Contact/>);
+    
+        const inputBox=screen.getAllByRole("textbox");
+    console.log(inputBox.length);
+    
+    expect(inputBox.length).toBe(2);
+    })
+});
 
-    const heading=screen.getByRole("heading");
 
-    expect(heading).toBeInTheDocument();
-})
+//describe is used to group test cases
 
-test("Should load contact or not",()=>{
-    render(<Contact/>);
 
-    const button=screen.getByRole("button");
 
-    expect(button).toBeInTheDocument();
-})
-
-test("Should load contact or not",()=>{
-    render(<Contact/>);
-
-    const button1=screen.getByText("Submit");
-
-    expect(button1).toBeInTheDocument();
-})
-
-test("Should load contact or not",()=>{
-    render(<Contact/>);
-
-    const inputBox=screen.getAllByRole("textbox");
-console.log(inputBox.length);
-
-expect(inputBox.length).toBe(2);
-})
